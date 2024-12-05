fa0e1b69cf21c02dd48d99131581c052fa5049a3
psaicho <guzekt@gmail.com>
2024-12-05 10:21:32 +0100

    add cookies to headers.js

--- florist/static/js/header.js
+++ florist/static/js/header.js
@@ -1,11 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
 
+    // Sprawdzanie stanu przy ładowaniu
+    const darkModeCookie = getCookie('darkMode');
+    if (darkModeCookie === 'true') {
+        body.classList.add('dark-mode');
+        if (darkModeToggle) {
+            darkModeToggle.checked = true;
+        }
+    }
+
     if (darkModeToggle) {
         darkModeToggle.addEventListener('click', () => {
             body.classList.toggle('dark-mode');
+            const isDarkMode = body.classList.contains('dark-mode');
+            document.cookie = `darkMode=${isDarkMode};path=/;max-age=${30*24*60*60};SameSite=Strict`;
         });
     }
 });
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("", include('florist.urls')),
     path('admin/', admin.site.urls),
     path('florist/', include('florist.urls')),
 ]
